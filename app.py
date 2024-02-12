cde25357f4cf8caf9beba084b65a402761ef4871
thf-k <157975175+thf-k@users.noreply.github.com>
2024-02-12 15:08:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+
 import openai
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, Response
@@ -8,27 +9,17 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def  home():
     return render_template('index.html')
 
-# une fonction qui prend en parametre la liste des echanges(getChatHist)
-# et qui renvoie une liste de dictionnaire le format attendu par openai !
-# annotation de type en entree -> sortie
 
-@app.route("/prompt", methods = ["POST"])
+@app.route('/prompt', methods = ["POST"])
 def prompt():
     messages = request.json['messages']
     conversation = build_conversation_dict(messages)
 
     return Response(event_stream(conversation), mimetype='text/event-stream')
-
-
-
-
-
-
-
 
 
 def build_conversation_dict(messages: list) -> list[dict]:
@@ -52,6 +43,3 @@ def event_stream(conversation : list[dict]) -> str:
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
-    conversation = build_conversation_dict(messages = ["Bonjour, comment ca va ?", "ca va et toi", "Super merci !"])
-    for line in event_stream(conversation):
-        print(line)
